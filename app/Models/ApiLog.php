ea5e24dea65cbb1563e4e2d727a5e94a5c8c88f7
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-08 13:34:41 +0300

    Update ApiLog.php

--- app/Models/ApiLog.php
+++ app/Models/ApiLog.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ApiLog extends Model
 {
     use HasFactory;
+
+    protected $table = 'api_data';
+    protected $fillable = ['request', 'response'];
+
 }
