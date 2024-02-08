fcb291339896915006df080c30df9e07ef21bcef
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2024-02-08 13:29:53 +0300

    Update ApiController.php
    Следующая версия.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,36 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\APIService;
+use App\Services\DatabaseService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\ApiLog;
 
 class ApiController extends Controller
 {
-    public function sendRequest(Request $request)
+    private $apiConnection;
+
+    public function __construct(APIService $apiConnection)
     {
-        // Получаем данные из запроса
+        $this->apiConnection = $apiConnection;
+    }
+
+    public function sendRequestAndDisplayResponse(Request $request)
+    {   $apiData = new ApiLog();
+        // Получение данных из запроса
         $requestData = $request->all();
 
-        // Преобразуем запрос в JSON объект
-        $requestJson = json_encode($requestData);
+        // Отправка запроса к API
+        $response = $this->apiConnection->sendRequest($requestData['url'], $requestData['data']);
 
-        // Отправляем запрос на API
-        $response = Http::post('https://api.example.com/endpoint', $requestData);
+        if($response  == "success")
+        {
+            $apiData->request = json_encode($requestData);
+            $apiData->response = $response;
+            $apiData->save();
+        }
+        // Вывод ответа на экран
+        return response()->json(['response' => $response]);
 
-        // Получаем ответ от API
-        $responseData = $response->json();
-
-        // Преобразуем ответ в JSON объект
-        $responseJson = json_encode($responseData);
-
-        // Сохраняем запрос и ответ в базе данных
-        $apiLog = new ApiLog;
-        $apiLog->request = $requestJson;
-        $apiLog->response = $responseJson;
-        $apiLog->save();
-
-        // Возвращаем ответ клиенту
-        return response()->json($responseData);
+        // Сохранение запроса и ответа в базе данных
+        
     }
 }
