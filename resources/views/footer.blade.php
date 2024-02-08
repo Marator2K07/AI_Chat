4c49413c74b87b1fd354786702c440af2e16f093
shik07 <119669266+shik07@users.noreply.github.com>
2024-02-08 18:37:38 +0700

    Add files via upload

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="card">
+  <div class="card-footer">
+    <h1 style="margin-left: 45%">Ai-chat</h1>
+  </div>
+  <div class="card-body" style="height: 60px">
+    <ul>
+    </ul>
+  </div>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,5 @@
+<div class="card">
+  <div class="card-header">
+   <h1 style="margin-left: 45%">Ai-chat</h1>
+  </div>
+</div>
